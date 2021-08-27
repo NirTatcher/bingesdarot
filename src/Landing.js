48ee879feac07504344d36bb55ceaf2ca33bfc82
nir <nirto111@gmail.com>
2021-08-27 16:07:39 +0300

    initial commit

---
+++ src/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StepperInstructions from './StepperInstructions'
+
+export default function Landing() {
+    return (
+        <div className="wrapper">
+
+            <div className="conetent">
+                content
+                <StepperInstructions />
+            </div>
+        </div>
+    )
+}
